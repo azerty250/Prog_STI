859de5a991decff03e66c3d7b148596b0367ac8d
azerty250 <36883459+azerty250@users.noreply.github.com>
2018-05-06 11:49:48 +0200

    v2_particle_filter
    J'ai recupéré les fonctions d'un mec en ligne. Encore quelques modif à faire mais le gros est là

---
+++ particle_filter_v2.py
@@ -0,0 +1,117 @@
+######################
+# Code base coming from https://github.com/rlabbe 
+# It was adapted to the characteristics of the robot
+#######################
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.random import uniform
+
+def create_uniform_particles(x_range, y_range, hdg_range, N):
+    particles = np.empty((N, 3))
+    particles[:, 0] = uniform(x_range[0], x_range[1], size=N)
+    particles[:, 1] = uniform(y_range[0], y_range[1], size=N)
+    particles[:, 2] = uniform(hdg_range[0], hdg_range[1], size=N)
+    particles[:, 2] %= 2 * np.pi
+    return particles
+
+def create_gaussian_particles(mean, std, N):
+    particles = np.empty((N, 3))
+    particles[:, 0] = mean[0] + (randn(N) * std[0])
+    particles[:, 1] = mean[1] + (randn(N) * std[1])
+    particles[:, 2] = mean[2] + (randn(N) * std[2])
+    particles[:, 2] %= 2 * np.pi
+    return particles
+
+
+def predict(particles, u, std, dt=1.):
+    """ move according to control input u (heading change, velocity)
+    with noise Q (std heading change, std velocity)`"""
+
+    N = len(particles)
+    # update heading
+    particles[:, 2] += u[0] + (randn(N) * std[0])
+    particles[:, 2] %= 2 * np.pi
+
+    # move in the (noisy) commanded direction
+    dist = (u[1] * dt) + (randn(N) * std[1])
+    particles[:, 0] += np.cos(particles[:, 2]) * dist
+    particles[:, 1] += np.sin(particles[:, 2]) * dist
+
+
+def update(particles, weights, z, R, landmarks):
+    weights.fill(1.)
+    for i, landmark in enumerate(landmarks):
+        distance = np.linalg.norm(particles[:, 0:2] - landmark, axis=1)
+        weights *= scipy.stats.norm(distance, R).pdf(z[i])
+
+    weights += 1.e-300      # avoid round-off to zero
+    weights /= sum(weights) # normalize
+
+
+def estimate(particles, weights):
+    """returns mean and variance of the weighted particles"""
+
+    pos = particles[:, 0:2]
+    mean = np.average(pos, weights=weights, axis=0)
+    var  = np.average((pos - mean)**2, weights=weights, axis=0)
+    return mean, var
+
+def simple_resample(particles, weights):
+    N = len(particles)
+    cumulative_sum = np.cumsum(weights)
+    cumulative_sum[-1] = 1. # avoid round-off error
+    indexes = np.searchsorted(cumulative_sum, random(N))
+
+    # resample according to indexes
+    particles[:] = particles[indexes]
+    weights.fill(1.0 / N)
+
+
+def neff(weights):
+    return 1. / np.sum(np.square(weights))
+
+
+
+# Main function
+def run_pf1(N, iters=18, sensor_std_err=.1,
+            xlim=(0, 20), ylim=(0, 20),
+            initial_x=None):
+    #Redefine the landmarks to be the position coming from the sensor
+    #This will determine the weights of the particles
+    landmarks = np.array([[-1, 2], [5, 10], [12,14], [18,21]])
+    NL = len(landmarks)
+    
+    plt.figure()
+    # Il faudrait deplacer dans un bout de code executé qu'une seule dans le main en dehors de la boucle principale
+    # create particles and weights near the initial position since we know where the robot starts
+    if initial_x is not None:
+        particles = create_gaussian_particles(mean=initial_x, std=(5, 5, np.pi/4), N=N)
+    else:
+        particles = create_uniform_particles((0,1), (0,1), (0, 6.28), N)
+
+    weights = np.zeros(N)
+    
+    xs = []
+    robot_pos = np.array([0., 0.])
+    for x in range(iters):
+        robot_pos += (1, 1)
+
+        # distance from robot to each landmark
+        zs = (norm(landmarks - robot_pos, axis=1) + 
+              (randn(NL) * sensor_std_err))
+
+        # move diagonally forward to (x+1, x+1)
+        predict(particles, u=(0.00, 1.414), std=(.2, .05))
+        
+        # incorporate measurements
+        update(particles, weights, z=zs, R=sensor_std_err, 
+               landmarks=landmarks)
+        
+        # resample if too few effective particles
+        if neff(weights) < N/2:
+            indexes = systematic_resample(weights)
+            resample_from_index(particles, weights, indexes)
+
+        # The mean is the estimated position of the robot --> most probable one
+        mu, var = estimate(particles, weights)
+        xs.append(mu)

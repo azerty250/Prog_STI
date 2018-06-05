77cf299578f1aba7617560d871b94616efacd0e3
gwickiepfl <gilles.wicki@epfl.ch>
2018-06-06 00:25:55 +0200

    recherche par zones d'angle

--- loc_real_time.py
+++ loc_real_time.py
@@ -56,16 +56,26 @@ def masque(im_input):
 	cv2.circle(msq, (326,210), 95, (0,0,0), -1)
 	
 	im_output = cv2.bitwise_and(im_input, im_input, mask = msq)
-	cv2.imshow("masquage", im_output)
+	#cv2.imshow("masquage", im_output)
+	return im_output
+
+#masquage dependant de l'angle precedemment calcule
+def masque_select(im_input, alpha):
+	alpha = -alpha*360/(2*np.pi)+180
+	msq = np.zeros((len(image),len(image[0])),np.uint8)
+	cv2.ellipse(msq,(326,210),(120,120),alpha,-250,70,(255,255,255),-1)
+	msq = 255-msq
+	cv2.imshow("msq",msq)
+	im_output = cv2.bitwise_and(im_input, im_input, mask = msq)
 	return im_output
 
 def opening(im_input):
 	kernel = np.ones((3,3),np.uint8)
 	erosion = cv2.erode(output,kernel,iterations = 1)
-	cv2.imshow("erosion",erosion)
+	#cv2.imshow("erosion",erosion)
 	
 	dilation = cv2.dilate(erosion,kernel,iterations = 1)
-	cv2.imshow("dilation", dilation)
+	#cv2.imshow("dilation", dilation)
 	return dilation
 	
 #function displaying the result
@@ -91,9 +101,19 @@ args = vars(ap.parse_args())
 
 # Choix du peripherique de capture. 0 pour /dev/video0.
 #cap = cv2.VideoCapture(0)
-stream = io.BytesIO()
+
 camera = picamera.PiCamera()
 camera.resolution = (640,480)
+#camera.framerate = 2
+#camera.brightness = 60
+
+stream = io.BytesIO()
+camera.capture(stream,format='jpeg')
+data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+image = cv2.imdecode(data, 1)
+
+angle = np.zeros((4,1))
+angle_prev = np.zeros((4,1))
 
 
 # definition des bornes pour le choix des couleurs
@@ -101,17 +121,20 @@ boundaries = [
 	([160, 50, 80], [10, 255, 255], 0), #rouge
 	([70,100,100],[100,255,255], 1), #vert
 	([80, 80, 80], [120, 255, 255], 2), #bleu
-	([0, 0, 50], [45, 200, 255], 3) #jaune
+	([10, 0, 150], [30, 100, 255], 3) #jaune
 ]
+
 
 while True:
 	print("passage")
+	
 	# On capture une image
 	#ret,image = cap.read()
+	stream = io.BytesIO()
 	camera.capture(stream,format='jpeg')
 	data = np.fromstring(stream.getvalue(), dtype=np.uint8)
 	image = cv2.imdecode(data, 1)
-	cv2.imshow("entree",image)
+	#cv2.imshow("entree",image)
 	image_msq = masque(image)
 
 	image_hsv = cv2.cvtColor(image_msq,  cv2.COLOR_BGR2HSV)
@@ -120,33 +143,41 @@ while True:
 	center_y = 198 #len(image)/2
 
 
-
-	angle = np.zeros((4,1))
-
 	# loop over the boundaries
 	for (lower, upper, num) in boundaries:
 	
 		# create NumPy arrays from the boundaries
 		lower = np.array(lower, dtype = "uint8")
 		upper = np.array(upper, dtype = "uint8")
- 
+		
+		if angle[num] != 0:
+			image_hsv_passage = masque_select(image_hsv,angle[num])
+		else:
+			image_hsv_passage = image_hsv
+		
+		masquee = cv2.cvtColor(image_hsv_passage, cv2.COLOR_HSV2BGR)
+		cv2.imshow("masque", masquee)
+		
 		if lower[0] > upper[0]:
 			# find the colors within the specified boundaries and apply
 			# the mask
-			mask1 = cv2.inRange(image_hsv, np.array([0, lower[1], lower[2]], dtype = "uint8"), upper)
-			mask2 = cv2.inRange(image_hsv, lower, np.array([255, upper[1], upper[2]], dtype = "uint8"))
+			mask1 = cv2.inRange(image_hsv_passage, np.array([0, lower[1], lower[2]], dtype = "uint8"), upper)
+			mask2 = cv2.inRange(image_hsv_passage, lower, np.array([255, upper[1], upper[2]], dtype = "uint8"))
 			mask = mask1 | mask2
 		else:
-			mask = cv2.inRange(image_hsv, lower, upper)
+			mask = cv2.inRange(image_hsv_passage, lower, upper)
 		
-		output_hsv = cv2.bitwise_and(image_hsv, image_hsv, mask = mask)
+		output_hsv = cv2.bitwise_and(image_hsv_passage, image_hsv_passage, mask = mask)
 
 		output = cv2.cvtColor(output_hsv, cv2.COLOR_HSV2BGR)
 
 		cv2.imshow("couleurs", output)
 
 		output = opening(output)
-	
+		angle_prev[num] = angle[num]
+		print("premier:")
+		print(angle[num])
+		print(angle_prev[num])
 		y, x, _ = np.nonzero(output)
 		if(len(x) != 0 and len(y) != 0):
 			tan = float(-(x[0]-center_x))/float(-(y[0]-center_y))
@@ -157,16 +188,23 @@ while True:
 	
 		# show the images
 		cv2.imshow("images", np.hstack([image, output]))
+		print("deuxieme:")
+		print(angle[num])
+		print(angle_prev[num])
 		cv2.waitKey(0)
+		
 
 
 	#compute the robot position and orientation using ToTal algorithm
 	position_x, position_y, theta = ToTal_algorithm((0,1),(0,0),(1,0),angle[0],angle[1],angle[2])
 	#print(angle)
+	#print(angle_prev)
 	print(theta)
 	print(position_x)
 	print(position_y)
-
-	dessin(position_x, position_y, theta)
-	
 	cv2.waitKey(0)
+	
+
+	#dessin(position_x, position_y, theta)
+	
+	#cv2.waitKey(0)
